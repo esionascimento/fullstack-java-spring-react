2cf943c6f309a8414e6ed40243b74724b0cb00ea
esionascimento <esionascimento@proton.me>
2024-02-12 15:15:05 -0400

    first commit

---
+++ src/main/java/com/example/cardapio/controller/FooterController.java
@@ -0,0 +1,23 @@
+package com.example.cardapio.controller;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FooterController {
+    @Autowired
+    private FoodRepository repository;
+
+    @GetMapping
+    public List getAll(){
+        List<Food> foodList = repository.findAll();
+        return foodList;
+    }
+}
